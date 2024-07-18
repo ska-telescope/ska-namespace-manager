ca9d0421b991d7834f148af0411741baf5dd2ba2
Pedro Osório <pedroosorio.eeic@gmail.com>
2024-07-18 15:00:58 +0100

    ST-2025: Initial setup

---
+++ src/ska_namespace_manager/main.py
@@ -0,0 +1,6 @@
+"""
+SKA Namespace Manager is a tool designed to efficiently manage namespaces in a
+Kuberentes cluster. The main goal is to be able to provide fair usage quotas
+to the multiple users of the cluster, aswell as to maintain efficient use of
+it.
+"""
